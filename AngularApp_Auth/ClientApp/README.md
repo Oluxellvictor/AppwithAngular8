7018f3e56799d2cf0106190a6a19606ab00776d8
oluxell.voa007@gmail.com <oluxell.voa007@gmail.com>
2019-10-27 01:26:37 -0700

    New Commit for navbar, products and registration page

--- AngularApp_Auth/ClientApp/README.md
+++ AngularApp_Auth/ClientApp/README.md
@@ -1,6 +1,6 @@
 # ClientApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.10.
 
 ## Development server
 
--- AngularApp_Auth/ClientApp/src/app/app.module.ts
+++ AngularApp_Auth/ClientApp/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavMenuComponent,
     LoginComponent,
-    NavMenuComponent
+    RegisterComponent,
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
